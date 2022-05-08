ec6b7fbd232cb504b9c7b84f33a8a1a06709e760
SOLO KWAN Léa <38751015+leaSoloKwan@users.noreply.github.com>
2022-05-08 13:34:54 +0200

    introduction exception handling

---
+++ src/main/java/com/springboot/web/controller/ErrorController.java
@@ -0,0 +1,23 @@
+package com.springboot.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("error")
+public class ErrorController {
+	
+
+	    @ExceptionHandler(Exception.class)
+	    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+	      
+	        ModelAndView mav = new ModelAndView();
+	        mav.addObject("exception", ex.getStackTrace());
+	        mav.addObject("url", req.getRequestURL());
+	        mav.setViewName("error");
+	        return mav;
+	    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,7 @@
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navigation.jspf" %>
+<div class="container">
+
+	Exception please contact support
+	</div>
+<%@ include file="common/footer.jspf" %>
